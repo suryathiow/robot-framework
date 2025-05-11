abe2866eab3759b1aa51caf1b20dc955bafa6609
suryathiow <suryathionow@gmail.com>
2025-05-11 15:03:21 +0700

    Add test data

---
+++ resources/test_data.py
@@ -0,0 +1,8 @@
+# Login Test Data
+valid_email = "[CHANGE_THIS]"
+valid_password = "[CHANGE_THIS]"
+
+invalid_email = "wasadoppp.com"
+invalid_password = "Handph1"
+
+not_registered_email = "not_registered@email.com"
